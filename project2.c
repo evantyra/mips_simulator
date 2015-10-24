7da82c66e54eccffbd64b7f1b0b8dfaf0a3395ef
psul26 <prsulliv@umass.edu>
2015-10-23 21:42:16 -0400

    created syntax function called in progScanner

--- project2.c
+++ project2.c
@@ -14,10 +14,52 @@ char *** instruct;
 
 int lineCount;
 
-void syntax(char * str){
-int commaCount;
-int paranCount;
+void syntax(char * instruction, char * opcode)
+{
+    printf("reached \n");
+    printf("string length = %i \n",strlen(instruction));
+    printf("instruction to syntax = %s \n",instruction);
+    int i;
+    int commaCount=0;
+    int paranLeftCount=0;
+    int paranRightCount=0;
+    for(i=0; i<strlen(instruction); i++)
+    {
+        if(instruction[i]==',')
+        {
+            commaCount++;
 
+        }
+
+        else if(instruction[i]=='(')
+        {
+            paranLeftCount++;
+        }
+
+        else if(instruction[i]==')')
+        {
+            paranRightCount++;
+        }
+
+
+    }
+    printf(" opcode = %s \n",opcode);
+    printf(" comma count = %i \n",commaCount);
+    printf(" paran left count = %i \n", paranLeftCount);
+    printf(" paran right count = %i \n", paranRightCount);
+    if (strcmp("lw",opcode)==0||strcmp("sw",opcode)==0)
+    {
+        assert(commaCount==1);
+        assert(paranLeftCount==1);
+        assert(paranRightCount==1);
+
+    }
+    if (strcmp("addi",opcode)==0||strcmp("add",opcode)==0||strcmp("sub",opcode)==0||strcmp("mult",opcode)==0||strcmp("beq",opcode)==0)
+    {
+        assert(paranLeftCount==0);
+        assert(paranRightCount==0);
+        assert(commaCount==2);
+    }
 }
 
 void progScanner()
@@ -43,7 +85,8 @@ void progScanner()
     int i = 0;
     int j = 0;
     instruct = (char ***)malloc(lines*sizeof(char *));
-    for (i = 0; i < lines; i++) {
+    for (i = 0; i < lines; i++)
+    {
         *(instruct+i) = (char **)malloc(4*sizeof(char *));
         for(j = 0; j < 4; j++)
         {
@@ -55,12 +98,14 @@ void progScanner()
     i=0;
     while(fgets(inputString, 100, instructionFile)>0)
     {
-        syntax(inputString);
+        char * copyInputString = (char *) malloc(100*sizeof(char));
         char delimiters[]=", ";
+        strcpy(copyInputString,inputString);
 
 
         printf("inputString[]=%s\n", inputString);
         strcpy(instruct[i][0] , strtok(inputString, delimiters));
+        syntax(copyInputString, instruct[i][0]);
         strcpy(instruct[i][1] , strtok(NULL, delimiters));
         strcpy(instruct[i][2] , strtok(NULL, delimiters));
         strcpy(instruct[i][3] , strtok(NULL, delimiters));
@@ -68,8 +113,7 @@ void progScanner()
 //        instruct[i][2] = strtok(NULL, delimiters);
 //        instruct[i][3] = strtok(NULL, delimiters);
 
-        printf("field 1 = %s field 2 = %s field 3 = %s field 4 = %s \n",instruct[i][0], instruct[i][1], instruct[i][2],instruct[i][3]);
-        printf(" the value of i be %i \n",i);
+
         i++;
     }
 
@@ -110,7 +154,7 @@ int main()
 {
     progScanner();
     int i;
-     for(i=0; i<lineCount; i++)
+    for(i=0; i<lineCount; i++)
     {
         printf("field 1  = %s field 2 = %s field 3 = %s field 4 = %s \n",instruct[i][0], instruct[i][1], instruct[i][2],instruct[i][3]);
     }
