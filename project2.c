9c39ea9b9f3cb89b1ebc210fb7c6c455550f8805
Evan Tyra <evantyra@gmail.com>
2015-10-25 19:53:58 -0400

    regNumberConverter inclusion
    regNumberConverter now works fully. With this commit I also added a new
    function of registerToNumber which is called by regNumberConverter to
    determine the register index and whether it is valid.

--- project2.c
+++ project2.c
@@ -50,10 +50,6 @@ void syntax(char * instruction, char * opcode)
             paranRightCount++;
         }
     }
-    printf(" opcode = %s \n", opcode);
-    printf(" comma count = %i \n", commaCount);
-    printf(" paran left count = %i \n", paranLeftCount);
-    printf(" paran right count = %i \n", paranRightCount);
 
     if (strcmp("lw",opcode) == 0 || strcmp("sw",opcode) == 0)
     {
@@ -61,7 +57,7 @@ void syntax(char * instruction, char * opcode)
         assert(paranLeftCount == 1);
         assert(paranRightCount == 1);
     }
-    if (strcmp("addi",opcode) == 0 || strcmp("add",opcode) == 0 || 
+    else if (strcmp("addi",opcode) == 0 || strcmp("add",opcode) == 0 || 
         strcmp("sub",opcode) == 0 || strcmp("mult",opcode) == 0 ||
         strcmp("beq",opcode) == 0)
     {
@@ -74,11 +70,10 @@ void syntax(char * instruction, char * opcode)
 // Reads line by line and looks for syntax
 char *progScanner(char* inputString)
 {
-    char *tempLine;
-    tempLine = (char *)malloc(100*sizeof(char *));
+    char *tempLine = (char *)malloc(100*sizeof(char *));
     char *space = " ";
 
-    char delimiters[]=", ()";
+    char delimiters[] = ", ()";
 
     char *copyInputString = (char *)malloc(100*sizeof(char *));
     strcpy(copyInputString, inputString);
@@ -94,6 +89,150 @@ char *progScanner(char* inputString)
     memcpy(&tempLine[strlen(tempLine)], strtok(NULL, delimiters), 100);
 
     return tempLine;
+}
+
+// Number of if and else ifs to determine whether an inputted register 
+char *registerToNumber(char* registerString) {
+    if (strcmp("$0", registerString) == 0 || strcmp("$zero", registerString) == 0)
+        return "0";
+    else if (strcmp("$1", registerString) == 0 || strcmp("$at", registerString) == 0)
+        return "1";
+    else if (strcmp("$2", registerString) == 0 || strcmp("$v0", registerString) == 0)
+        return "2";
+    else if (strcmp("$3", registerString) == 0 || strcmp("$v1", registerString) == 0)
+        return "3";
+    else if (strcmp("$4", registerString) == 0 || strcmp("$a0", registerString) == 0)
+        return "4";
+    else if (strcmp("$5", registerString) == 0 || strcmp("$a1", registerString) == 0)
+        return "5";
+    else if (strcmp("$6", registerString) == 0 || strcmp("$a2", registerString) == 0)
+        return "6";
+    else if (strcmp("$7", registerString) == 0 || strcmp("$a3", registerString) == 0)
+        return "7";
+    else if (strcmp("$8", registerString) == 0 || strcmp("$t0", registerString) == 0)
+        return "8";
+    else if (strcmp("$9", registerString) == 0 || strcmp("$t1", registerString) == 0)
+        return "9";
+    else if (strcmp("$10", registerString) == 0 || strcmp("$t2", registerString) == 0)
+        return "10";
+    else if (strcmp("$11", registerString) == 0 || strcmp("$t3", registerString) == 0)
+        return "11";
+    else if (strcmp("$12", registerString) == 0 || strcmp("$t4", registerString) == 0)
+        return "12";
+    else if (strcmp("$13", registerString) == 0 || strcmp("$t5", registerString) == 0)
+        return "13";
+    else if (strcmp("$14", registerString) == 0 || strcmp("$t6", registerString) == 0)
+        return "14";
+    else if (strcmp("$14", registerString) == 0 || strcmp("$t7", registerString) == 0)
+        return "14";
+    else if (strcmp("$15", registerString) == 0 || strcmp("$s0", registerString) == 0)
+        return "15";
+    else if (strcmp("$16", registerString) == 0 || strcmp("$s1", registerString) == 0)
+        return "16";
+    else if (strcmp("$17", registerString) == 0 || strcmp("$s2", registerString) == 0)
+        return "18";
+    else if (strcmp("$19", registerString) == 0 || strcmp("$s3", registerString) == 0)
+        return "19";
+    else if (strcmp("$20", registerString) == 0 || strcmp("$s4", registerString) == 0)
+        return "20";
+    else if (strcmp("$21", registerString) == 0 || strcmp("$s5", registerString) == 0)
+        return "21";
+    else if (strcmp("$22", registerString) == 0 || strcmp("$s6", registerString) == 0)
+        return "22";
+    else if (strcmp("$23", registerString) == 0 || strcmp("$s7", registerString) == 0)
+        return "23";
+    else if (strcmp("$24", registerString) == 0 || strcmp("$t8", registerString) == 0)
+        return "24";
+    else if (strcmp("$25", registerString) == 0 || strcmp("$t9", registerString) == 0)
+        return "25";
+    else if (strcmp("$26", registerString) == 0 || strcmp("$k0", registerString) == 0)
+        return "26";
+    else if (strcmp("$27", registerString) == 0 || strcmp("$k1", registerString) == 0)
+        return "27";
+    else if (strcmp("$28", registerString) == 0 || strcmp("$gp", registerString) == 0)
+        return "28";
+    else if (strcmp("$29", registerString) == 0 || strcmp("$sp", registerString) == 0)
+        return "29";
+    else if (strcmp("$30", registerString) == 0 || strcmp("$fp", registerString) == 0)
+        return "30";
+    else if (strcmp("$31", registerString) == 0 || strcmp("$ra", registerString) == 0)
+        return "31";
+    else {
+        printf("Register %s is invalid - Simulator Stopped\n", registerString);
+        exit(1);
+    }
+}
+
+char *regNumberConverter(char* inputString) {
+    char delimiters[] = " \n";
+    char *space = " ";
+    int i;
+
+    char *returnLine = (char *)malloc(100*sizeof(char *));
+
+    // Check for the operation type
+    memcpy(returnLine, strtok(inputString, delimiters), 100);
+
+    if (strcmp("sw", returnLine) == 0 || strcmp("lw", returnLine) == 0) {
+        // Register
+        memcpy(&returnLine[strlen(returnLine)], space, 100);
+        memcpy(&returnLine[strlen(returnLine)], registerToNumber(strtok(NULL, delimiters)), 100);
+
+        // Integer
+        char *number = strtok(NULL, delimiters);
+        for (i = 0; i < strlen(number); i++)
+            if (!isdigit(number[i]))
+            {
+                printf ("Expected element to be number, is not - Simulator Stopped\n");
+                exit(1);
+            }
+        memcpy(&returnLine[strlen(returnLine)], space, 100);
+        memcpy(&returnLine[strlen(returnLine)], number, 100);
+
+        // Register
+        memcpy(&returnLine[strlen(returnLine)], space, 100);
+        memcpy(&returnLine[strlen(returnLine)], registerToNumber(strtok(NULL, delimiters)), 100);
+
+        return returnLine;
+    }
+    else if (strcmp("addi", returnLine) == 0 || strcmp("beq", returnLine) == 0) {
+        // Register
+        memcpy(&returnLine[strlen(returnLine)], space, 100);
+        memcpy(&returnLine[strlen(returnLine)], registerToNumber(strtok(NULL, delimiters)), 100);
+
+        // Register
+        memcpy(&returnLine[strlen(returnLine)], space, 100);
+        memcpy(&returnLine[strlen(returnLine)], registerToNumber(strtok(NULL, delimiters)), 100);
+
+        // Integer
+        char *number = strtok(NULL, delimiters);
+        for (i = 0; i < strlen(number); i++)
+            if (!isdigit(number[i]))
+            {
+                if (i == 0 && number[i] == '-')
+                    continue;
+                printf ("Expected element to be number, is not - Simulator Stopped\n");
+                exit(1);
+            }
+        memcpy(&returnLine[strlen(returnLine)], space, 100);
+        memcpy(&returnLine[strlen(returnLine)], number, 100);
+
+        return returnLine;
+    }
+
+    // Register
+    memcpy(&returnLine[strlen(returnLine)], space, 100);
+    memcpy(&returnLine[strlen(returnLine)], registerToNumber(strtok(NULL, delimiters)), 100);
+
+    // Register
+    memcpy(&returnLine[strlen(returnLine)], space, 100);
+    memcpy(&returnLine[strlen(returnLine)], registerToNumber(strtok(NULL, delimiters)), 100);
+
+    // Register
+    memcpy(&returnLine[strlen(returnLine)], space, 100);
+    memcpy(&returnLine[strlen(returnLine)], registerToNumber(strtok(NULL, delimiters)), 100);
+
+    return returnLine;
 }
 
 int main()
@@ -137,6 +276,7 @@ int main()
     }
 
     for (i = 0; i < lineCount; i++) {
+        instructions[i] = regNumberConverter(instructions[i]);
         printf("%s\n", instructions[i]);
     }
 
